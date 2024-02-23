fa37f64fb0ed35b94e55af0e7d8626b2c3fbfa6d
akash-panthri <akashpanthri2000@gmail.com>
2024-02-24 04:32:59 +0530

    Created Menuitem component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,14 @@
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+      <Header />
+      {children}
+      </body>
     </html>
-  )
+  );
 }
