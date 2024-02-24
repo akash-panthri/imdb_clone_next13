060478e5fbd5d57ccc748625e6be0d7398627859
akash-panthri <akashpanthri2000@gmail.com>
2024-02-25 04:21:12 +0530

    Added dark mode option

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,15 @@
 import Header from "@/components/Header";
 import "./globals.css";
+import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
       <body>
+    <Providers>
       <Header />
       {children}
+      </Providers>
       </body>
     </html>
   );
