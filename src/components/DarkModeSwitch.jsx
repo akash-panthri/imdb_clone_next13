f45b17bd870fe751fc658557112c236da3641ca7
akash-panthri <akashpanthri2000@gmail.com>
2024-02-25 04:32:15 +0530

    Made dark mode to mount only on first render

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -2,12 +2,17 @@
 import { MdLightMode } from "react-icons/md";
 import { BsFillMoonFill } from "react-icons/bs";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 export default function DarkModeSwitch() {
-    const { systemTheme, theme, setTheme } = useTheme();
-    const currentTheme = theme === "system" ? systemTheme : theme;
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
   return (
     <>
-{currentTheme === "dark" ? (
+      {mounted &&
+        (currentTheme === "dark" ? (
           <MdLightMode
             className="text-xl cursor-pointer hover:text-amber-500"
             onClick={() => setTheme("light")}
@@ -17,7 +22,7 @@ export default function DarkModeSwitch() {
             className="text-xl cursor-pointer hover:text-amber-500"
             onClick={() => setTheme("dark")}
           />
-        )}
+        ))}
     </>
-  )
+  );
 }
