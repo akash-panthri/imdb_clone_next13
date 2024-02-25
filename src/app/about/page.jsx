54b407994e00d876c01923ffde85e568788177c1
akash-panthri <akashpanthri2000@gmail.com>
2024-02-26 04:22:25 +0530

    Styled about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,7 @@
 export default function About() {
   return (
-    <div>About</div>
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+    </div>
   );
 }
