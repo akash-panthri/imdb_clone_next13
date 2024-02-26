b8e2ae2135bf7549da4d95a0e7fe1386fa53fc22
akash-panthri <akashpanthri2000@gmail.com>
2024-02-27 04:02:08 +0530

    Added Navbar component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import Providers from "./Providers";
+import Navbar from "@/components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
       <body>
     <Providers>
       <Header />
+      <Navbar />
       {children}
       </Providers>
       </body>
