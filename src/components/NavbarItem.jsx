c892a28f73e7f6e109e83ebc362c633df7ae0f8c
akash-panthri <akashpanthri2000@gmail.com>
2024-02-27 04:17:13 +0530

    Added NavbarItem component

---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NavbarItem() {
+  return (
+    <div>
+      <Link
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  )
+}
