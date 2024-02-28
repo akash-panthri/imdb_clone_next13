5a3b32b6b72f9ce2bdc902202ad0bef7b2f1f408
akash-panthri <akashpanthri2000@gmail.com>
2024-02-29 04:06:22 +0530

    Added fetch function to page.jsx in app directory

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,17 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+const API_KEY = process.env.API_KEY;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
 
-const inter = Inter({ subsets: ['latin'] })
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
 
-export default function Home() {
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
   return (
     <>
       <h1 className="text-3xl font-bold underline">
