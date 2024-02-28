27e9e9dc0dc79bc807244975d09d04b2082a10c8
akash-panthri <akashpanthri2000@gmail.com>
2024-02-29 04:24:52 +0530

    Created Results component and rendered movie titles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+import Results from "@/components/Results";
+
 const API_KEY = process.env.API_KEY;
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
@@ -12,10 +14,15 @@ export default async function Home({ searchParams }) {
   if (!res.ok) {
     throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
   }
+
+    const data = await res.json();
+
+    const results = data.results;
+    console.log(results)
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <Results results={results} />
     </h1>
     </>
   )
