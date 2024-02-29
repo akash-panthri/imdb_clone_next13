2e873bce597bd2b93e66c9880236c56c2a6fc0dd
akash-panthri <akashpanthri2000@gmail.com>
2024-03-01 05:17:10 +0530

    Modified loading.jsx

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function loading() {
   return (
-    <div>loading</div>
+    <div className="flex justify-center">
+      <img className="h-96" src="spinner.svg" alt="loading..." />
+    </div>
+
   )
 }
