e0dcd600a31769a08a37f65a12fd860cf293e233
akash-panthri <akashpanthri2000@gmail.com>
2024-03-04 04:51:30 +0530

    Added data to SearchBox Component.

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,12 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function SearchBox() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!search) return;
+    router.push(`/search/${search}`);
+  }
   return (
-    <form className="flex max-w-6xl mx-auto justify-between items-center px-5">
+    <form
+      onSubmit={handleSubmit}
+      className="flex max-w-6xl mx-auto justify-between items-center px-5"
+    >
       <input
         type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         placeholder="Search keywords..."
         className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"
       />
       <button
+        disabled={!search}
         type="submit"
         className="text-amber-600 disabled:text-gray-400"
       >
