37969a7c37d15ff0d758ce159fb4446fc48a8e48
akash-panthri <akashpanthri2000@gmail.com>
2024-03-04 04:24:01 +0530

    Styled SearchBox Component

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,6 +1,17 @@
-
 export default function SearchBox() {
   return (
-    <div>SearchBox</div>
-  )
+    <form className="flex max-w-6xl mx-auto justify-between items-center px-5">
+      <input
+        type="text"
+        placeholder="Search keywords..."
+        className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"
+      />
+      <button
+        type="submit"
+        className="text-amber-600 disabled:text-gray-400"
+      >
+        Search
+      </button>
+    </form>
+  );
 }
